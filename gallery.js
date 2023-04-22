0d33ed998e6f420bd6716b48b8fcf8811d13a021
63darmoo <129257437+63darmoo@users.noreply.github.com>
2023-04-22 12:51:29 -0700

    changes

--- gallery.js
+++ gallery.js
@@ -5,6 +5,6 @@ ScrollReveal({
   delay: 400
 });
 
-ScrollReveal().reveal('.column1', { delay: 600 });
-ScrollReveal().reveal('.column2', { delay: 700 });
-ScrollReveal().reveal('.column3', { delay: 800 });
+ScrollReveal().reveal('.column1', { delay: 300 });
+ScrollReveal().reveal('.column2', { delay: 400 });
+ScrollReveal().reveal('.column3', { delay: 500 });
